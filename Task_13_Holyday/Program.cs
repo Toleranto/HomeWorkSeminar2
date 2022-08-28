28c597517525cdb52c18c5db5e9c940852f5f867
Eugenius <eugenius8447@gmail.com>
2022-08-28 17:19:59 +0300

    First name

---
+++ Task_13_Holyday/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа 
+//или сообщает, что третьей цифры нет.
+Console.Clear();
+Console.Write("Введите число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+string NumberText = Convert.ToString(Number);
+if(NumberText.Length > 2)
+{
+    Console.WriteLine("Третья цифра: "+NumberText[2]); 
+}
+else
+{
+   Console.WriteLine("Третьей цифры нет"); 
+} 
