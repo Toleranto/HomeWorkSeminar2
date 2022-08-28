7b01bc29113945aa8c28197c94c790f3dc828d13
Eugenius <eugenius8447@gmail.com>
2022-08-28 16:06:55 +0300

    Task10

---
+++ Task10_Three Numbers/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.Write("Введи трёхзначное число: ");
+        int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+        string stringNumber = Convert.ToString(threeDigitNumber);
+        Console.WriteLine("Вторая цифра числа: " + stringNumber[1]);
+    }
+}
